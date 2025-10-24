bf72ee8db77aef1ccd4302f9e9251e1b048fdd21
souahrani-khaldi <ouahraniprv@gmail.com>
2025-10-24 16:42:17 +0200

    Changement MAJEUR et final de la classe : changement de la disposition des entiers dans les conteneur et verifs k

--- src/main/java/gl/bp/BitPackingAligned.java
+++ src/main/java/gl/bp/BitPackingAligned.java
@@ -1,77 +1,71 @@
+// Classe BitPackingAligned - OUAHRANI KHALDI Sofiane
 package gl.bp;
 
-public class BitPackingAligned extends BitPacking{
+public class BitPackingAligned extends BitPacking {
     @Override
     public int[] compress(int[] input) {
-        // on calcule k (nb de bits de l'entier le plus grand tu tableau)
-        this.calculateK(input);
-        int K = this.k;
 
-        /*if (this.k == 0 ) {
-            return new int[0];
-        }*/
-        // combien d'entiers de k bits tiennent dans 32 bits
+        this.calculateK(input); //mtn k est init
+
+        if (this.k == 0 ) {
+            return new int[0]; // au cas ou le tableau est vide
+        }
+
+        int K = this.k; // juste pour plus de lisibilité
+
         final int ITEMS_PER_CONTAINER = INT_BITS / K;
 
+        // taille tableau de sortie en nb de 32bits
         int compressedSize = (int) Math.ceil((double) this.originalSize / ITEMS_PER_CONTAINER); // j'ai fais la div en flottant pour eviter des trucs du genre 3/2 = 1
         int[] compressedArray = new int[compressedSize];
 
         // Variables que j'utilise pour le suivi
         int outputIndex = 0; // Index du tableau de sortie
-        int bitOffset = 0;   // Décalage en bits dans compressedArray[outputIndex]
+        int bitOffset = 0;   // position de bit actuelle en partant de la droite (bit 0)
 
-        //boucle principale (inputVal = chaque valeur d'entrée)
-        for (int inputVal :input){
-            //on vérifie que le prochain paquet de k bits rentre dans le conteneur
-            //sinon bourrage et on passe au prochain conteneur
-            if(bitOffset + K > INT_BITS){
+        for (int inputVal : input) {
+            // je vérifie si l'ajout du prochain paquet ne fit pas dans le conteneur -> conteneur suivant
+            if (bitOffset + K > INT_BITS) {
                 outputIndex++;
-                bitOffset = 0; // décalage remis à 0 car je passe au prochain conteneur
+                bitOffset = 0;
             }
 
-            // = distance entre le bit 0 à droite et le début du paquet
-            int positionnement = INT_BITS - K - bitOffset;
 
-            // je positionne le paquet par un décalage à gauche et je l'insère avec l'op OU binaire
-            compressedArray[outputIndex] |= (inputVal << positionnement);
+            compressedArray[outputIndex] |= (inputVal << bitOffset); /* je décale inputVal du nb de bits nécessaire vers la gauche comme ça
+            mes prochains nombres s'ajouterons à la suite jusqu'a changement du conteneur et comme ça je ne risquerais pas de modif les nb deja placés,
+            et je l'ajoute au tableau compressé avec le OU BINAIRE*/
 
-
-            // mise à jour du décalage (k bits ducoup)
+            // ducoup K bits supp sont occupés
             bitOffset += K;
         }
 
-
         //tableau compressé final :)
         return compressedArray;
-
     }
 
     @Override
     public int[] decompress(int[] compressedArray) {
         int K = this.k;
 
-        /*
+        // CAS OU K n'est pas init !!!
         if (K == 0) {
-            throw new IllegalStateException("k n'est pas initialisé.");
-        }*/
+            throw new IllegalStateException("k n'est pas initialisé : merci d'appeler compress() sur le tableau d'abord.");
+        }
 
         int[] outputArray = new int[this.originalSize];
-
 
         int indexContainer = 0;
         int bitOffset = 0;
 
-
-        for (int i = 0; i<this.originalSize;i++){
-            if (bitOffset + K > INT_BITS){
-                indexContainer ++;
-                bitOffset =0;
+        for (int i = 0; i < this.originalSize; i++) {
+            if (bitOffset + K > INT_BITS) { //dès que c le cas soit padding soit tout lus
+                indexContainer++;
+                bitOffset = 0;
             }
-            // decalage vers la droit pour amener les k bits au bit 0
-            int decalage = INT_BITS - K - bitOffset;
+
 
             // décalage à droite
-            outputArray[i] = (compressedArray[indexContainer] >> decalage) & ((1 << K) - 1);
+            outputArray[i] = (compressedArray[indexContainer] >>> bitOffset) & ((1 << K) - 1); //décalage a droite et isolement avec masque pour avoir des zéro partout sauf mon int
             bitOffset += K;
         }
 
@@ -81,35 +75,27 @@ public class BitPackingAligned extends BitPacking{
 
     @Override
     public int get(int[] compressedArray, int i) {
-
-        // GERER LE CAS OU K N'EST PAS INITIALISE
         final int K = this.k;
 
-        /*if (K == 0) {
-            throw new IllegalStateException("k n'est pas initialisé.");
-        }*/
+        // c'est pas cool on stop direct
+        if (K == 0) {
+            throw new IllegalStateException("k n'est pas initialisé : merci d'appeler compress() sur le tableau d'abord.");
+        }
 
-        // je pense que le nom est assez explicite..
+
+        // nb d'elt par conteneur max
         int itemsPerContainer = INT_BITS / K;
 
         // index du conteneur contenant l'entier i
         int indexContainer = i / itemsPerContainer;
 
-        // index de l'item dans le conteneur ( 0 : itemsPerContainer-1)
+        // index de l'item dans le conteneur
         int itemInContainer = i % itemsPerContainer;
 
         // Position du bit de départ du i-ème élément (0 pour le bit de poids fort)
         int bitOffset = itemInContainer * K;
 
-        /* isoler kbits qui nous intéresse dans le mot de 32 bit et le transformer en entier
-         >> décale vers la droite par un nombre de positions que nous calculons pour que
-         notre nombre soit tout à droite ( 32 bits - k bits - pos de départ)
-         + masque ! (j'ai chois de soustraire 1 au masque pour n'avoir que des 1 pour touts les bits à droite, et ducoup on aura des
-         0 à gauche ce qui permettra d'ignorer les éventuels entiers a gauche de notre nombre (PB) */
-
-        int decalage = INT_BITS - K - bitOffset;
-        int result = (compressedArray[indexContainer] >> decalage) & ((1 << K) - 1);
+        int result = (compressedArray[indexContainer] >>> bitOffset) & ((1 << K) - 1);
         return result;
-
     }
 }
